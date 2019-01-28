3873a11d6d54c132b4af6e65755687edb6488f4f
Olga Soldatenkova <olka030389@yandex.ru>
2019-01-28 23:53:40 +0300

    Add Task 1

---
+++ Task 1/CommandLineParams.java
@@ -0,0 +1,11 @@
+package start;
+
+public class CommandLineParams {
+	public static void main(String[] args) {
+		for(int i=0; i<args.length; i++) { 
+		System.out.println("Argument " + i + "=" + args[i]);
+		
+		}
+	}
+	
+}
---
+++ Task 1/Test.java
@@ -0,0 +1,16 @@
+package core;
+
+public class Test {
+ public static void main(String[] args) {
+	 int a = 48;
+	 short b = 100;
+	 long c = 10000;
+	 double d = 1026.25;
+	 float dt = 25.16f;
+	 char e = 'n';
+	 boolean f = true;
+	 System.out.println(a + " " + b + " " + c + " " + d + " " + dt + " " + e + " " + f);
+	 
+ }
+ 
+}

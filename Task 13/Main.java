33dbb7ce1578b0978744fc2404783031c37b842a
Olga Soldatenkova <olka030389@yandex.ru>
2019-02-17 18:20:48 +0300

    add Task 13.txt

---
+++ Task 13/Main.java
@@ -0,0 +1,14 @@
+package core;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Massive massive = new Massive();
+		massive.recordMas();
+		massive.findElementByIndex(10);
+		massive.swapElementsInMas(10);
+		massive.checkedElementsMas();
+	}
+
+}
